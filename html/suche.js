b8f0574961cd342ca0a3ed469e99ff0621f7d6af
Carsten Milling <cmil@hashtable.de>
2007-12-19 10:33:31 +0000

    Entwurf für Suchmaske hinsichtlich Accessibility überarbeitet:
    - in gültiges XHTML überführt sowie CSS und Javascript ausgelagert
    
    - Hilfetexte sind jetzt auch ohne JS erreichbar
    
    - jedes Eingabefeld hat ein Label, das ihm per ID zugeordnet ist;
      Verwendung von Fieldsets, wo angebracht
    
    - CSS weitgehend von px auf em umgestellt; weniger floats; neue und
      weniger Klassennamen (kein camelCase, mehr Semantik)
    
    - JS erzeugt (und befüllt) jetzt eine eigene Hilfe-Box, die neben dem
      Formular angezeigt wird

---
+++ html/suche.js
@@ -0,0 +1,39 @@
+var formId = 'suchformular';
+var helpSectionId = 'hilfe';
+var helpBoxId = 'js-hilfe';
+
+function initHelp () {
+    var form = document.getElementById(formId);
+    if(!form) return false;
+
+    var helpSection = document.getElementById(helpSectionId);
+    if(!helpSection) return false;
+    helpSection.style.display = 'none';
+
+    var helpBox = document.createElement('div');
+    helpBox.setAttribute('id', helpBoxId);
+    form.appendChild(helpBox);
+
+    var links = form.getElementsByTagName('a');
+    for (var i = 0; i < links.length; i++) {
+        var a = links[i];
+        var id = a.getAttribute('href').substr(1);
+
+        a.onclick = function(e) {
+            helpBox.style.display =
+                helpBox.style.display == 'none' ? 'block' : 'none';
+            return false;
+        };
+
+        a.onmouseover = function(e) {
+            var id = this.getAttribute('href').substr(1);
+            var help = document.getElementById(id);
+            if(help) {
+                helpBox.innerHTML = help.innerHTML;
+                helpBox.style.display = 'block';
+            }
+        };
+    }
+}
+
+window.onload = initHelp;
