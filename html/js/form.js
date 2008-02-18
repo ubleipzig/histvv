409645ab255a4d9c95c7b8fdfcf11e8e076738a5
Carsten Milling <cmil@hashtable.de>
2008-02-18 15:44:41 +0000

    Replaced home grown help system with jQuery tooltips.
    There are currently some difficulties in Opera, where the tooltips are
    always positioned above the cursor. This is problematic with tooltips
    near the upper edge of the view port. Chances are that this is fixable
    by tweaking our CSS since the tooltips demo doesn't show this
    behavior.

--- html/js/form.js
+++ html/js/form.js
@@ -1,41 +1,12 @@
 var formId = 'suchformular';
 var helpSectionId = 'hilfe';
-var helpBoxId = 'js-hilfe';
 
-function initHelp () {
-    var form = document.getElementById(formId);
-    if(!form) return false;
-
-    var helpSection = document.getElementById(helpSectionId);
-    if(!helpSection) return false;
-    helpSection.style.display = 'none';
-
-    var helpBox = document.createElement('div');
-    helpBox.setAttribute('id', helpBoxId);
-    form.appendChild(helpBox);
-
-    var links = form.getElementsByTagName('a');
-    for (var i = 0; i < links.length; i++) {
-        var a = links[i];
-        var id = a.getAttribute('href').substr(1);
-
-        a.onclick = function(e) {
-            helpBox.style.display =
-                helpBox.style.display == 'none' ? 'block' : 'none';
-            return false;
-        };
-
-        a.onmouseover = function(e) {
-            var id = this.getAttribute('href').substr(1);
-            var help = document.getElementById(id);
-            if(help) {
-                if(helpBox.firstChild) helpBox.removeChild(helpBox.firstChild);
-                helpContent = help.cloneNode(true);
-                helpBox.appendChild(helpContent);
-                helpBox.style.display = 'block';
-            }
-        };
-    }
-}
-
-window.onload = initHelp;
+$(document).ready(function(){
+    $('#'+helpSectionId).hide();
+    $('#'+formId+' a').tooltip({
+        bodyHandler: function() {
+            return $($(this).attr('href')).html();
+        },
+        showURL: false
+    });
+});
