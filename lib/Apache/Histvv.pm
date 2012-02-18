b35745a93c2ccd636e259e0ae9f8314229d3b476
Carsten Milling <cmil@hashtable.de>
2012-02-18 00:07:03 +0000

    bumped Histvv version to 0.09

--- lib/Apache/Histvv.pm
+++ lib/Apache/Histvv.pm
@@ -9,11 +9,11 @@ Apache::Histvv - the Histvv Apache handler
 
 =head1 VERSION
 
-Version 0.08
+Version 0.09
 
 =cut
 
-our $VERSION = '0.08';
+our $VERSION = '0.09';
 
 =head1 SYNOPSIS
 
--- lib/Histvv/Search.pm
+++ lib/Histvv/Search.pm
@@ -16,11 +16,11 @@ Histvv::Search - Histvv search related methods
 
 =head1 VERSION
 
-Version 0.08
+Version 0.09
 
 =cut
 
-our $VERSION = '0.08';
+our $VERSION = '0.09';
 
 =head1 SYNOPSIS
 
