248b6c04bacc3c71f6610c23bd7703288210639c
Carsten Milling <cmil@hashtable.de>
2012-08-31 23:35:13 +0000

    implemented gndlinkPrepend
    If set to true links will be inserted before existing ones.

--- share/htdocs/lib/gndlink.js
+++ share/htdocs/lib/gndlink.js
@@ -1,3 +1,7 @@
+var gndlinkPndPara = 'p#pnd';
+var gndlinkURL = '/gndlink/';
+var gndlinkPrepend = false;
+
 $(document).ready(function(){
     if (gndlinkPndPara && gndlinkURL) {
         var pnd = $(gndlinkPndPara + ' a').text();
@@ -22,16 +26,25 @@ function gndlinks (pnd) {
             list = $('#content.dozent ul.links');
         }
 
+        var firstItem;
         list.find('li').each(function(index) {
             var text = $.trim( $(this).text() ).replace(/\s\s+/g, ' ');
             links.push( text );
+            if (index === 0) firstItem = $(this);
         });
 
         for (var i in data) {
-            if ($.inArray(data[i]['label'], links) >= 0) continue;
-            list.append(
-                '<li><a href="' + data[i]['url'] + '">'
-                    + data[i]['label'] + '</a></li>' );
+            if ($.inArray(data[i].label, links) >= 0) continue;
+
+            var item = '<li><a href="' + data[i].url + '">'
+                + data[i].label + '</a></li>';
+
+            if (gndlinkPrepend && firstItem) {
+                firstItem.before(item);
+            }
+            else {
+                list.append(item);
+            }
         }
     });
 }
