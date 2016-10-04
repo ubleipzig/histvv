28896f282ee099299810b6028977679a62dde169
Carsten Milling <cmil@hashtable.de>
2016-10-04 14:52:49 +0200

    implement routes

--- app.js
+++ app.js
@@ -1,10 +1,37 @@
+'use strict';
+
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
+var basex = require('basex');
+
+var config = require('./config.json');
+
+var session = new basex.Session();
+session.execute('OPEN ' + config.db.name, function (err, r) {
+  if (err) {
+    console.error(err);
+  }
+  console.log(r.info);
+});
+
+var routeHandlerFactory = require('./routehandler.js')(session);
 
 var app = express();
 
+app.set('strict routing', true);
+
 app.use(logger('dev'));
+
+app.get('/dozenten/', routeHandlerFactory('dozenten.xq', 'dozenten.xsl'));
+app.get('/dozenten/galerie.html', routeHandlerFactory('dozenten.xq', 'dozenten.xsl'));
+app.get('/dozenten/namen.html', routeHandlerFactory('dozentennamen.xq', 'dozenten.xsl'));
+app.get('/dozenten/lookup/:name', routeHandlerFactory('dozentenlookup.xq', 'dozenten.xsl'));
+app.get('/dozenten/:id.html', routeHandlerFactory('dozent.xq', 'dozenten.xsl'));
+app.get('/suche/', routeHandlerFactory('suchformular.xq', 'suche.xsl'));
+app.get('/vv/', routeHandlerFactory('index.xq', 'vv.xsl'));
+app.get('/vv/:id.html', routeHandlerFactory('semester.xq', 'vv.xsl'));
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 
---
+++ routehandler.js
@@ -0,0 +1,59 @@
+'use strict';
+
+var path = require('path');
+var fs = require('fs');
+var libxslt = require('libxslt');
+
+var xqydir = 'xqy';
+var xsldir = 'xsl';
+
+function loadFile (filename, dir) {
+  var file = /^\//.test(filename)
+    ? filename
+    : path.join(__dirname, dir, filename);
+  return fs.readFileSync(file, 'utf-8');
+}
+
+module.exports = function (dbSession) {
+
+  return function (xqyFile, xslFile) {
+
+    var xqy = loadFile(xqyFile, xqydir);
+    var xsl = loadFile(xslFile, xsldir);
+    // fix include path's to satisfy node-libxslt
+    // see https://github.com/albanm/node-libxslt#includes
+    xsl = xsl.replace(
+      /xsl:import href="/g,
+      'xsl:import href="' + xsldir + '/'
+    );
+
+    var stylesheet = libxslt.parse(xsl);
+    var query = dbSession.query(xqy);
+
+    function routeHandler (req, res, next) {
+
+      // bind route params to the query
+      Object.keys(req.params).forEach(function (name) {
+        query.bind(name, req.params[name], '', console.log);
+      });
+
+      // stylesheet params
+      var xslparams = {
+        'histvv-url': req.originalUrl
+      };
+
+      query.execute(function (err, r) {
+        if (err) console.log(err);
+        if (r.result === '') {
+          next();
+          return;
+        }
+        var html = stylesheet.apply(r.result, xslparams);
+        res.set('Content-type', 'text/html');
+        res.send(html);
+      });
+    }
+
+    return routeHandler;
+  };
+};
