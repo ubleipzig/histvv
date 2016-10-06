57b3792ddc0506f7b142d90365bd0d297dd572eb
Carsten Milling <cmil@hashtable.de>
2016-10-06 02:52:30 +0200

    issue plain text errors in production for now

--- app.js
+++ app.js
@@ -73,10 +73,8 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.json({
-    message: err.message,
-    error: {}
-  });
+  res.type('text');
+  res.send(err.message);
 });
 
 
